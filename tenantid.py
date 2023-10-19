aea00dbc1174132e943047761b75c85a8bc25a22
Beretta8045 <147774522+Beretta8045@users.noreply.github.com>
2023-10-19 10:50:55 -0400

    Update tenantid.py

--- tenantid.py
+++ tenantid.py
@@ -1,16 +1,17 @@
-from azure.identity import ManagedIdentityCredential  
+from azure.identity import DefaultAzureCredential  
 from azure.keyvault.secrets import SecretClient  
   
-# Set your Azure resource group name, Key Vault name, and secret name  
-resource_group_name = 'your_resource_group_name'  
-key_vault_name = 'your_key_vault_name'  
-secret_name = 'your_secret_name'  
+# Set your Azure subscription ID, resource group name, Key Vault name, and secret name  
+subscription_id = '4a11049c-f1ef-4b33-8fdc-000845f3b37c'  
+resource_group_name = 'Marketplaceoffer'  
+key_vault_name = 'gtgkeyvault2'  
+secret_name = 'TENANT_ID_SECRET_NAME'  
   
-# Create a ManagedIdentityCredential object  
-credential = ManagedIdentityCredential()  
+# Create a DefaultAzureCredential object  
+credential = DefaultAzureCredential()  
   
 # Retrieve the Azure Tenant ID  
-tenant_id = credential.get_token("https://management.azure.com/.default").tenant_id  
+tenant_id = credential.get_token("https://vault.azure.net/.default").tenant_id  
   
 # Create a SecretClient object  
 secret_client = SecretClient(vault_url=f"https://{key_vault_name}.vault.azure.net", credential=credential)  
