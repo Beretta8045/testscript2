139f8933d351a60350f7c0681f96b1db25eaa4e7
Beretta8045 <147774522+Beretta8045@users.noreply.github.com>
2023-10-19 11:07:32 -0400

    Update tenantid.py

--- tenantid.py
+++ tenantid.py
@@ -1,5 +1,6 @@
 from azure.identity import DefaultAzureCredential  
 from azure.keyvault.secrets import SecretClient  
+from jwt import decode
 
 # Set your Azure subscription ID, resource group name, Key Vault name, and secret name  
 subscription_id = '4a11049c-f1ef-4b33-8fdc-000845f3b37c'  
@@ -10,9 +11,11 @@ secret_name = 'TENANT_ID_SECRET_NAME'
 # Create a DefaultAzureCredential object  
 credential = DefaultAzureCredential()  
 
-# Retrieve the Azure Tenant ID  
+# Retrieve the Azure Tenant ID
 token = credential.get_token("https://management.azure.com/.default")
-tenant_id = token.tenant_id  # Access tenant_id directly
+access_token = token.token
+decoded_token = decode(access_token, verify=False)
+tenant_id = decoded_token['tid']
 
 # Create a SecretClient object  
 secret_client = SecretClient(vault_url=f"https://{key_vault_name}.vault.azure.net", credential=credential)  
