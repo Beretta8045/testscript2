0b439ccb699c41734d91eb7be1f3290d8fe15cad
Beretta8045 <147774522+Beretta8045@users.noreply.github.com>
2023-10-19 10:41:49 -0400

    Update tenantid.py

--- tenantid.py
+++ tenantid.py
@@ -1,38 +1,22 @@
-from azure.identity import DefaultAzureCredential
-from azure.keyvault.secrets import SecretClient
-import os
-
-
-# Define the Key Vault URL and the name for the secret that will store the tenant ID
-TENANT_ID_SECRET_NAME = "azure-ad-tenant-id"
-VAULT_URL = os.environ["https://gtgkeyvault2.vault.azure.net"]
-credential = DefaultAzureCredential()
-secret_client = SecretClient(vault_url="https://gtgkeyvault1.vault.azure.net", credential=credential)
-secret = secret_client.set_secret("secret-name", "secret-value")
-
-# Use Managed Identity to authenticate to Azure Key Vault
-credential = DefaultAzureCredential()
-secret_client = SecretClient(vault_url=KEY_VAULT_URL, credential=credential)
-
-# Capture the Azure AD Tenant ID for the current subscription
-try:
-    tenant_id = os.environ["AZURE_TENANT_ID"]
-    print("Captured Azure AD Tenant ID: %s" % tenant_id)
-
-    # Store the Tenant ID in the Key Vault as a secret
-    secret_client.set_secret(TENANT_ID_SECRET_NAME, tenant_id)
-    print("Tenant ID stored in Key Vault as secret: {}".format(TENANT_ID_SECRET_NAME))
-except KeyError:
-    print("Azure AD Tenant ID is not available in the environment variables.")
-
-# Capture the Azure AD Tenant Name for the current subscription
-try:
-    tenant_name = os.environ["AZURE_AD_TENANT_NAME"]
-    print("Captured Azure AD Tenant Name: %s" % tenant_name)
-
-    # Store the Tenant Name in the Key Vault as a secret
-    secret_name = "azure-ad-tenant-name"  # Name for the secret
-    secret_client.set_secret(secret_name, tenant_name)
-    print("Tenant Name stored in Key Vault as secret: {}".format(secret_name))
-except KeyError:
-    print("Azure AD Tenant Name is not available in the environment variables.")
+from azure.identity import DefaultAzureCredential  
+from azure.keyvault.secrets import SecretClient  
+  
+# Set your Azure subscription ID, resource group name, Key Vault name, and secret name  
+subscription_id = '4a11049c-f1ef-4b33-8fdc-000845f3b37c'  
+resource_group_name = 'Marketplaceoffer'  
+key_vault_name = 'gtgkeyvault2'  
+secret_name = 'TENANT_ID_SECRET_NAME'  
+  
+# Create a DefaultAzureCredential object  
+credential = DefaultAzureCredential()  
+  
+# Retrieve the Azure Tenant ID  
+tenant_id = credential.get_token("https://management.azure.com/.default").tenant_id  
+  
+# Create a SecretClient object  
+secret_client = SecretClient(vault_url=f"https://{key_vault_name}.vault.azure.net", credential=credential)  
+  
+# Store the Tenant ID in the Key Vault as a secret  
+secret_client.set_secret(secret_name, tenant_id)  
+  
+print(f"Azure Tenant ID '{tenant_id}' stored in Key Vault '{key_vault_name}' as secret '{secret_name}'")  
