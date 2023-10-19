29286ccb20ed78bf859851842fee9589e59d9ca4
Beretta8045 <147774522+Beretta8045@users.noreply.github.com>
2023-10-19 14:29:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,32 @@ from passlib.hash import sha256_crypt
 import psycopg2
 import random
 import string
+import secrets
 
 app = Flask(__name)
-app.secret_key = secrets.token_hex(32)
-
-# ... (previous code for the Flask app)
+app.secret_key = secrets.token_hex(32)  # Set a secret key for session management
 
 # Function to check if a user is logged in
 def is_logged_in():
     return 'logged_in' in session
 
+# Database configuration (replace with your database details)
+db_config = {
+    'database': 'your_database',
+    'user': 'your_user',
+    'password': 'your_password',
+    'host': 'your_database_host',
+    'port': 'your_database_port'
+}
+
+# License Key Generation
+def generate_license_key():
+    key_length = 20  # Adjust the length as needed
+    characters = string.ascii_letters + string.digits
+    license_key = ''.join(random.choice(characters) for _ in range(key_length))
+    return license_key
+
+# Routes
 @app.route("/", methods=["GET", "POST"])
 def login():
     if is_logged_in():
@@ -22,13 +38,7 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        connection = psycopg2.connect(
-            database="your_database",
-            user="your_user",
-            password="your_password",
-            host="your_database_host",
-            port="your_database_port"
-        )
+        connection = psycopg2.connect(**db_config)
         cursor = connection.cursor()
         cursor.execute("SELECT password FROM users WHERE username = %s", (username,))
         user_data = cursor.fetchone()
@@ -51,12 +61,21 @@ def logout():
 def index():
     if is_logged_in():
         if request.method == "POST":
-            # ... (license key generation and database storage as before)
+            # Generate and store the license key
+            license_key = generate_license_key()
+            
+            connection = psycopg2.connect(**db_config)
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO licenses (license_key) VALUES (%s)", (license_key,))
+            connection.commit()
+            connection.close()
+            
+            return render_template("index.html", license_key=license_key)
+        
         return render_template("index.html")
     else:
         return redirect(url_for("login"))
 
-# ... (rest of the code for the Flask app)
-
 if __name__ == "__main__":
     app.run(debug=True)
+
