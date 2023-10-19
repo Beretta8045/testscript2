5688c631ad715cf7c924bd74e5eabee8e6796bff
Beretta8045 <147774522+Beretta8045@users.noreply.github.com>
2023-10-19 12:03:14 -0400

    Update tenantid.py

--- tenantid.py
+++ tenantid.py
@@ -1,26 +1,31 @@
-from azure.identity import DefaultAzureCredential  
-from azure.keyvault.secrets import SecretClient  
-from jwt import decode
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
-# Set your Azure subscription ID, resource group name, Key Vault name, and secret name  
-subscription_id = '4a11049c-f1ef-4b33-8fdc-000845f3b37c'  
-resource_group_name = 'Marketplaceoffer'  
-key_vault_name = 'gtgkeyvault2'  
-secret_name = 'MyTenantID'  # Replace with a valid secret name
+# Azure Key Vault details
+key_vault_name = 'gtgkeyvault2e'
+secret_name = 'TenantID'
 
-# Create a DefaultAzureCredential object  
-credential = DefaultAzureCredential()  
+# Use an existing user-assigned identity
+identity_client_id = 'gtgidentity'
 
-# Retrieve the Azure Tenant ID
-token = credential.get_token("https://management.azure.com/.default")
-access_token = token.token
-decoded_token = decode(access_token, verify=False)
-tenant_id = decoded_token['tid']
+# Read the Azure Tenant ID from the text file
+tenant_id = None
+with open('/path/to/your/config/file.txt', 'r') as config_file:
+    for line in config_file:
+        if line.startswith("Azure Tenant ID:"):
+            tenant_id = line.split(":")[1].strip()
+            break
 
-# Create a SecretClient object  
-secret_client = SecretClient(vault_url=f"https://{key_vault_name}.vault.azure.net", credential=credential)  
+if tenant_id is not None:
+    # Authenticate using the existing user-assigned identity
+    credential = DefaultAzureCredential(accepted_scopes=["https://vault.azure.net"])
 
-# Store the Tenant ID in the Key Vault as a secret  
-secret_client.set_secret(secret_name, tenant_id)  
+    # Create a SecretClient object
+    secret_client = SecretClient(vault_url=f"https://{key_vault_name}.vault.azure.net", credential=credential)
 
-print(f"Azure Tenant ID '{tenant_id}' stored in Key Vault '{key_vault_name}' as secret '{secret_name}'")
+    # Create or update the secret in the Key Vault
+    secret_bundle = secret_client.set_secret(secret_name, tenant_id)
+
+    print(f"Azure Tenant ID '{tenant_id}' stored in Key Vault '{key_vault_name}' as secret '{secret_name}'")
+else:
+    print("Azure Tenant ID not found in the configuration file.")
